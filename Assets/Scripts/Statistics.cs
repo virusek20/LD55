e92e6837c7cdbd52509a9d67df8c2fcc079f450a
virusek20 <gmoder.virusek20@gmail.com>
2024-04-14 18:01:38 +0200

    Add game end statistics

---
+++ Assets/Scripts/Statistics.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Statistics : MonoBehaviour
+{
+    public TextMeshProUGUI StatisticsText;
+    public ObjectiveManager Objective;
+
+    void Start()
+    {
+        ShowStats();
+    }
+
+    public void ShowStats()
+    {
+        Time.timeScale = 0f;
+
+        StatisticsText.text = $"Time: {Mathf.Floor(Time.timeSinceLevelLoad / 60)}m {Mathf.Ceil(Time.timeSinceLevelLoad) % 60}s\n" +
+            $"Objectives:\n" +
+            $"\tCompleted: {Objective.States.Count(s => s.IsCompleted)}/{Objective.States.Count(s => !s.Objective.CanFail)}\n" +
+            $"\tFailed: {Objective.States.Count(s => s.IsFailed)}/{Objective.States.Count(s => s.Objective.CanFail)}\n";
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
